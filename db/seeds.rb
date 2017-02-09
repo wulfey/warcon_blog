c01f2b721b5e99360280faf967e2057ae1b56a8b
Jason Wolfe <Jasonwolfe@gmail.com>
2017-02-09 05:11:57 +0000

    seed population and linking added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Article.create(title: "Max Skitarii", description: "4 dragoons, 3 walkers")
+Article.create(title: "SummoningCon", description: "Librarius Conclave with Daemons ")
+Article.create(title: "Cawltarri", description: "2 DragoonCon with Skitarri CAD")
+Article.create(title: "Haemotrope and Servitors", description: "Haemotropes serve as walls, servitors as targets")
+Article.create(title: "CastellonCon", description: "Use Castellon Robots as point sinks")
+Article.create(title: "Dual Knights", description: "Skimpy on skitarri, but heavy on the AV13")
