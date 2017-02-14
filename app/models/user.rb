9041a1e2792acf74a6902b7f85967a105646d367
Jason Wolfe <Jasonwolfe@gmail.com>
2017-02-14 03:08:23 +0000

    added users table and model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+
+
+    #   validates :title, presence: true, length: {minimum: 3, maximum: 50}
+    #   validates :description, presence: true, length: {minimum: 10, maximum: 1000}
+
+
+end
