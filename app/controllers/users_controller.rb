e4266f9a328c3c53e24a6ca350aa0181f83e3a03
Jason Wolfe <Jasonwolfe@gmail.com>
2017-02-19 01:28:45 +0000

    auto log in on create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save 
+        session[:user_id] = @user.id 
         flash[:success] = "Welcome to the WarCon blog #{@user.username}."
-        redirect_to articles_path
+        redirect_to user_path(@user)
     else
         render 'new'
     end
@@ -40,7 +41,7 @@ class UsersController < ApplicationController
   end
 
 
-  private 
+private 
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
