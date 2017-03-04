97a1e45ccf4f7edbf96931052ff8130bf6fa604e
Jason Wolfe <Jasonwolfe@gmail.com>
2017-03-04 01:20:54 +0000

    added AWS materials

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,12 @@
 class Image < ActiveRecord::Base
   belongs_to :user
   mount_uploader :picture, ImageUploader
+  validate :picture_size 
+  
+  private 
+    def picture_size
+        if picture.saize > 5.megabytes 
+            errors.add(:picture, "shoul dbe less than 5MB")
+        end
+    end
 end
